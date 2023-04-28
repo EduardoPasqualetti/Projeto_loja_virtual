4c611f2aab3d24768fc08f5abf70c9cfa97b4a49
Vinicius Dos Santos Grana <54106581833@info.local>
2023-04-28 10:22:11 -0300

    Criado classe boleto

---
+++ Boleto.cs
@@ -0,0 +1,20 @@
+
+namespace Projeto_loja_virtual
+{
+    public class Boleto
+    {
+        //propriedade
+        private string CodigoDeBarras;
+        // Metodo
+        public void Registrar()
+        {
+
+        }
+
+
+
+
+
+
+    }
+}
