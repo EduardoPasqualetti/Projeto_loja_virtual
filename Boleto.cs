da405de4908d97b299df7bcf9061f70c8e5e2a47
Vinicius Dos Santos Grana <54106581833@info.local>
2023-04-28 10:38:48 -0300

    parte 2

--- Boleto.cs
+++ Boleto.cs
@@ -3,13 +3,27 @@ namespace Projeto_loja_virtual
 {
     public class Boleto
     {
+
+
         //propriedade
         private string CodigoDeBarras;
+
+        public double desconto {get; set;}
+        public double valor {get; set;}
+
         // Metodo
         public void Registrar()
         {
 
         }
+
+        public void CalcularDesconto()
+        {
+            desconto = valor * 0.88;
+        }
+
+
+
 
 
 
