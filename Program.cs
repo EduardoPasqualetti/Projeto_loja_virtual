bd68d021419ccb4ab4dba4efe0f39ba79c00d915
Eduardo Pasqualetti <49802002810@info.local>
2023-05-03 11:01:30 -0300

    ajustes

--- Program.cs
+++ Program.cs
@@ -8,20 +8,16 @@ CartaoCredito cred = new CartaoCredito();
 
 string opcao;
 string denovo;
+float valorCompra;
 Console.ForegroundColor = ConsoleColor.DarkBlue;
 Console.WriteLine(@$"
 --------------------------------------
 |   Bem vindo a nossa loja virtual!  |
 --------------------------------------
 "); Console.ResetColor();
-do
-{
 
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine($"Informe o valor da compra:");
-    float valorCompra = float.Parse(Console.ReadLine());
-
-    Console.WriteLine(@$"
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(@$"
        Escolha a forma de Pagamento:
     ====================================
 
@@ -34,9 +30,12 @@ do
     
             Escolha uma Opcão: 
     ");
-    opcao = Console.ReadLine();
+opcao = Console.ReadLine();
 
-    Console.ResetColor();
+if (opcao != "0")
+{
+    Console.WriteLine($"Informe o valor da compra:");
+    valorCompra = float.Parse(Console.ReadLine());
 
     switch (opcao)
     {
@@ -60,14 +59,14 @@ do
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"As informacoes do cartao foram salvas!");
             Console.ResetColor();
-            
+
             deb.valor = valorCompra;
             Console.WriteLine($"");
             deb.Pagar();
             Console.WriteLine($"");
             break;
         case "3":
-                    Console.WriteLine($"Digite o nome do titular do cartao:");
+            Console.WriteLine($"Digite o nome do titular do cartao:");
             cred.Titular = Console.ReadLine();
             Console.WriteLine($"Informe a Bandeira do cartao:");
             cred.Bandeira = Console.ReadLine();
@@ -89,24 +88,120 @@ do
             Console.WriteLine($"{loja.Cancelar()}");
             Console.WriteLine($"");
             break;
-        case "0":
-            Console.WriteLine($"");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Saindo do sistema!");
-            Console.WriteLine($"");
-            Console.ResetColor();
-            break;
         default:
             Console.WriteLine($"");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Opao Invalida, Tente novamente e escolha uma opcao valida!");
+            Console.WriteLine($"Opcao Invalida, Tente novamente e escolha uma opcao valida!");
             Console.WriteLine($"");
             Console.ResetColor();
             break;
     }
 
+    do
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(@$"
+       Escolha a forma de Pagamento:
+    ====================================
 
-} while (opcao != "0");
+    1 - Pagamento em Boleto
+    2 - Pagamento em Cartão de Débito
+    3 - Pagamento em Cartão de Crédito
+    4 - Cancelar Operação
+
+    0 - Sair do Sistema
+    
+            Escolha uma Opcão: 
+    ");
+        opcao = Console.ReadLine();
+
+        if (opcao != "0")
+        {
+            Console.WriteLine($"Informe o valor da compra:");
+            valorCompra = float.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case "1":
+
+                    bol.valor = valorCompra;
+                    Console.WriteLine($"");
+                    bol.Registrar();
+                    Console.WriteLine($"");
+
+                    break;
+                case "2":
+                    Console.WriteLine($"Digite o nome do titular do cartao:");
+                    deb.Titular = Console.ReadLine();
+                    Console.WriteLine($"Informe a Bandeira do cartao:");
+                    deb.Bandeira = Console.ReadLine();
+                    Console.WriteLine($"Informe o numero do cartao:");
+                    deb.NumeroCartao = Console.ReadLine();
+                    Console.WriteLine($"Informe o CVV do cartao:");
+                    deb.Cvv = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"As informacoes do cartao foram salvas!");
+                    Console.ResetColor();
+
+                    deb.valor = valorCompra;
+                    Console.WriteLine($"");
+                    deb.Pagar();
+                    Console.WriteLine($"");
+                    break;
+                case "3":
+                    Console.WriteLine($"Digite o nome do titular do cartao:");
+                    cred.Titular = Console.ReadLine();
+                    Console.WriteLine($"Informe a Bandeira do cartao:");
+                    cred.Bandeira = Console.ReadLine();
+                    Console.WriteLine($"Informe o numero do cartao:");
+                    cred.NumeroCartao = Console.ReadLine();
+                    Console.WriteLine($"Informe o CVV do cartao:");
+                    cred.Cvv = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"As informacoes do cartao foram salvas!");
+                    Console.ResetColor();
+
+                    cred.valor = valorCompra;
+                    Console.WriteLine($"");
+                    cred.Pagar();
+                    Console.WriteLine($"");
+                    break;
+                case "4":
+                    Console.WriteLine($"");
+                    Console.WriteLine($"{loja.Cancelar()}");
+                    Console.WriteLine($"");
+                    break;
+                default:
+                    Console.WriteLine($"");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Opcao Invalida, Tente novamente e escolha uma opcao valida!");
+                    Console.WriteLine($"");
+                    Console.ResetColor();
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Saindo do sistema!");
+            Console.WriteLine($"");
+            Console.ResetColor();
+        }
+
+    } while (opcao != "0");
+}
+else
+{
+    Console.WriteLine($"");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Saindo do sistema!");
+    Console.WriteLine($"");
+    Console.ResetColor();
+}
+
+
+
 
 
 
